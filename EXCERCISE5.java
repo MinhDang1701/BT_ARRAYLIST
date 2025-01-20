b65bd66de492fea06dc48feaa5168eccffee218a
MinhDang1701 <minh82320@gmail.com>
2025-01-20 08:33:59 +0700

    Add files via upload

---
+++ EXCERCISE5.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ThinkPad
+ */
+public class EXCERCISE5 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+        numbers.add(60);
+        numbers.add(70);
+        numbers.add(80);
+        numbers.add(90);
+        numbers.add(100);
+
+        System.out.println("SO CHX BI TRU: " + numbers);
+
+        numbers.remove(3);
+        System.out.println("KQ TRU DI SO THU 3: " + numbers);
+    }
+}
